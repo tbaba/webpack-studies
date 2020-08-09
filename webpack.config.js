0ea1763f147ba99d9b793192cade8a8b3cca1846
Tatsuro Baba <harakirisoul@gmail.com>
2020-08-09 17:38:32 +0900

    Introduce sass-loader and node-sass

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,14 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
         test: /\.(jpe?g|png|gif|svg|ico)$/i,
         use: [
           {
